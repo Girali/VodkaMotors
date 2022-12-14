751df7b0c89684ede230039154fd5c8aa1f11ed5
Girali <thegirali@wp.pl>
2022-12-14 17:34:42 +0100

    pC SWITCH

--- Assets/WheelViewManger.cs
+++ Assets/WheelViewManger.cs
@@ -7,32 +7,20 @@ public class WheelViewManger : MonoBehaviour
     [SerializeField] 
     private GameObject render;
     private WheelCollider wheel;
-    [SerializeField] 
-    private AxeViewManager axe;
-    private Vector3 initPosition;
-    private Quaternion initRotation;
 
     public Vector3 v;
     public Quaternion q;
-    public Vector3 locaPosition;
 
     private void Start()
     {
         wheel= GetComponent<WheelCollider>();
-        initPosition = Vector3.zero;//render.transform.localPosition;
-        initRotation = Quaternion.identity;// render.transform.localRotation;
     }
 
     private void Update()
     {
         wheel.GetWorldPose(out v, out q);
 
-        render.transform.position = v + initPosition;
-        render.transform.rotation = q * initRotation;
-    }
-
-    private void OnDisable()
-    {
-        locaPosition = axe.vehicul.InverseTransformDirection(v - axe.transform.position);
+        render.transform.position = v;
+        render.transform.rotation = q;
     }
 }
